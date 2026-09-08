f28b524ffbfad84ba6fb111af00797112364bfae
saalsfwa <ai.pioneer.network@gmail.com>
2026-09-08 18:28:31 +0330

    Fix: Change base to root for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 ﻿import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://saalsfwa.github.io',
-  base: '/nexsyspersia/',
+  site: 'https://nexsyspersia.ir',
+  base: '/',
 });
